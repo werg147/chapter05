4eff622ac2b3ca27b7fdc07647a784f7c373501a
장세림 <@DESKTOP-1CIRGN6>
2020-12-14 18:00:53 +0900

    txt파일 읽고 List에 넣기/추가하기

--- src/com/javaex/ex04/PhoneApp.java
+++ src/com/javaex/ex04/PhoneApp.java
@@ -1,11 +1,15 @@
 package com.javaex.ex04;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class PhoneApp {
 
@@ -13,11 +17,18 @@ public class PhoneApp {
 		
 		//List 사용
 		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		//파일 읽어오기
 		Reader fr = new FileReader("C:\\javastudy\\PhoneDB.txt");
 		BufferedReader br = new BufferedReader(fr);
 		
-		List brList = new ArrayList();
+		//리스트 준비
+		List<Person> pList = new ArrayList<Person>();
 		
+		//리스트에 입력
 		while(true) {
 			String str = br.readLine();
 			if(str == null) {
@@ -25,11 +36,39 @@ public class PhoneApp {
 			}
 			
 			String[] data = str.split(",");
-			
+			Person person = new Person(data[0], data[1], data[2]);
+			pList.add(person);
 			
 		}
 		
+		System.out.println(pList.toString());
+		br.close();
 		
+		//정보 추가 (모든 정보를 새로쓰는 방식)
+		System.out.println("추가할 친구 정보를 입력하세요.");
+		String str = sc.nextLine();
+		String[] data = str.split(",");
+		
+		Person person = new Person(data[0], data[1], data[2]);
+		pList.add(person);
+		
+		System.out.println(pList.toString());
+		
+		//리스트의 내용을 파일에 쓰기.
+		//기존 데이터는 지워지고 정보를 다 새로 쓴다 -> 리스트의 정보를 모두 파일로 쓴다.
+		Writer fw = new FileWriter("C:\\javastudy\\PhoneDB.txt");
+		BufferedWriter bw = new BufferedWriter(fw);
+		
+		
+		for(Person p : pList) {
+			String pStr = p.getName() + "," + p.getHp() + "," + p.getCompany();
+			bw.write(pStr);
+			bw.newLine();
+		}
+		
+		
+		bw.close();
+		sc.close();
 		
 	}
 
